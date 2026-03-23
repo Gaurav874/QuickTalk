38a7f0a8c330df89ff1f9bbd56ffa48499751484
Gaurav <>
2026-03-23 18:56:49 +0530

    websocket working properly 7

--- backend/index.js
+++ backend/index.js
@@ -30,15 +30,23 @@ app.use('/api/message/', messageroutes)
 const dbConnect = require("./config/database");
 dbConnect()
 
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if(process.env.NODE_ENV === "production") {
+    // app.use(express.static(path.join(__dirname, "../frontend/dist")));
    
     // app.get("*", (req, res) => {
     //     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     // });
-    app.get("/(.*)", (req, res) => {
-        res.sendFile(path.join(__dirname, "..", "frontend", "dist", "index.html"));
-    });
+
+    // app.get("/(.*)", (req, res) => {
+    //     res.sendFile(path.join(__dirname, "..", "frontend", "dist", "index.html"));
+    // });
+// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
 server.listen(port,function (){
